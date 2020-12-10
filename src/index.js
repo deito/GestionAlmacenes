6a1eb043e6d7d36c7c2f7fec5080659f3d2e64e6
Diego Alvarez <deito.alvarez@gmail.com>
2020-12-10 18:25:01 -0500

    se agregó permisos CORS

--- src/index.js
+++ src/index.js
@@ -11,6 +11,15 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+//for cors
+app.use(function(req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+});
+
 // Routes
 app.use('/usuario',require('./route/usuarioRouter'));
 app.use('/local', require('./route/localRouter'));
